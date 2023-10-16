2f2f05e36bd3509bd4b20f67bb54fc821ebe6b54
trungbanh <trungbanh97@gmail.com>
2023-10-16 21:50:14 +0700

    navbar and assets

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import NavbarHome from './components/NavbarHome.vue'
+</script>
+
+<template>
+  <NavbarHome></NavbarHome>
+
+  <main class="main-bg main-container">
+
+  </main>
+</template>
+
+<style scoped>
+.text-rubik {
+  font-family: rubik;
+}
+
+.text-work_sans {
+  font-family: work_sans;
+}
+
+.main-bg {
+  background-color: #f4f8fa
+}
+
+.main-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
---
+++ src/components/NavbarHome.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="bg-white">
+    <nav class="flex items-center justify-between" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <a href="#" class="my-6 mx-14">
+          <img class="w-auto" src="@/assets/logoOrigin.svg" alt="" />
+        </a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
